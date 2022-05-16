cf4ad3827a0e6acdf52936e15cc1b1b7773c9f20
PHETpierre <36611009+PHETpierre@users.noreply.github.com>
2022-05-16 14:25:25 +0200

    start

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,19 @@
 f = open("test.txt", "r")
 
-print(f)
+specialChars = {
+    'newLine' : {'regRule' : '/\r\n/g', 'value' :'\n'},
+    'case' : {'regRule' : '/case/g', 'value' :'case'},
+    # endInstruct:    {regRule: /;/g,  value:';'},
+    # equal:          {regRule: /=/g, value:'='},
+    # point:           {regRule: /\./g, value:'.'},
+    # virgule:           {regRule: /\,/g, value:','},
+    # quotationMark:  {regRule: /\"/g, value:'"'},
+    # openParenthese:  {regRule: /\(/g, value:'"'},
+    # closeParenthese:  {regRule: /\)/g, value:'"'}
+}
+
+def hello():
+    print("yoyoyo")
+    print(specialChars['case']['regRule'])
+
+hello()

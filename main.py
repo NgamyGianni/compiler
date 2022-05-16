d70066ea3076688c76e560076c8a173057b176a0
PHETpierre <36611009+PHETpierre@users.noreply.github.com>
2022-05-16 15:39:20 +0200

    On branch case  Changes to be committed: 	modified:   main.py 	modified:   tokenizer/tokenizer.py 	modified:   tokenizer/tokenizer.pyc
     Changes not staged for commit:
    	deleted:    tokenizer.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from parser import parser
 from scoring import scoring
 
 f = open("test.js", "r")
-tokenizer.tokenizer(f)
+s = f.read()
+
+consts = tokenizer.constrKeyword(s, "const")
+
+print([e.toString() for e in consts])
+
+f.close()
+# tokenizer.tokenizer(f)
 parser.parser()
 scoring.scoring()
--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -17,8 +17,6 @@ def hello():
     print("yoyoyo")
     print(specialChars['case']['regRule'])
 
-hello()
-
 # keys = ["await", "break", "case", "catch", "class", "const", "continue", "debugger", "default",
 #         "delete", "do", "else", "enum", "export", "extends", "false", "finally", "for", "function",
 #         "if", "implements", "import", "in", "instanceof", "interface", "let", "new", "null", "package",
@@ -45,3 +43,41 @@ def tokenizer(file):
             # checkifInstruction(word)
     for token in tokens:
         print(token)
+
+class constant :
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+        if '"'in value:
+            self.type = "string"
+        else:
+            if '.' in value:
+                self.type = "float"
+            else:
+                self.type = "int"
+
+        self.struct = {"name" : self.name, "value" : self.value, "type" : self.type}
+
+    def toString(self):
+        return self.struct
+
+def stringToLines(s):
+    return [word.split(" ") for word in s.split("\n")]
+
+def linesToLine(lines):
+    return [word for line in lines for word in line]
+
+def lineToList(line, index):
+    return ''.join(line[index:])
+
+def checkKeyword(keyword, s):
+    lines = stringToLines(s)
+    return keyword in linesToLine(lines)
+
+def verifyLine(keyword, line):
+    return len(line) >= 4 and line[0] == keyword and line[2] == "=" and ";" in lineToList(line, 2)
+
+def constrKeyword(s, keyword):
+    return [constant(line[1], line[3].split(";")[0]) for line in stringToLines(s) if keyword in line]

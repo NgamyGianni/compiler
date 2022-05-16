a9a1c0c7a5294637549cb90ea63a60b1cd47f928
Ashanth <ashanth92@gmail.com>
2022-05-16 14:15:36 +0200

    first commit

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,26 @@
-f = open("test.txt", "r")
+f = open("test.js", "r")
 
-print(f)
+keys = ["await", "break", "case", "catch", "class", "const", "continue", "debugger", "default",
+        "delete", "do", "else", "enum", "export", "extends", "false", "finally", "for", "function",
+        "if", "implements", "import", "in", "instanceof", "interface", "let", "new", "null", "package",
+        "private", "protected", "public", "return", "super", "switch", "static", "this", "throw", "try",
+        "true", "typeof", "var", "void", "while", "with", "yield"]
+L = []
+
+Lines = f.readlines()
+  
+count = 0
+# Strips the newline character
+for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line.strip()))
+
+def checkifInstruction(code):
+    print(L)
+
+for sentence in f.read().split("\n"):
+    for word in sentence.split(" "):
+        if word in keys:
+            if(word=="if"):
+                L.append(word)
+                checkifInstruction(word)

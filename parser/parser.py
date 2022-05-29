a6e0b549caa6082dae47ef3be33477709dff0814
PHETpierre <36611009+PHETpierre@users.noreply.github.com>
2022-05-29 23:00:38 +0200

    Changes to be committed: 	modified:   parser/parser.py 	modified:   parser/parser.pyc 	modified:   scoring/__init__.pyc 	modified:   scoring/scoring.pyc 	modified:   tokenizer/__init__.pyc 	modified:   tokenizer/tokenizer.pyc

--- parser/parser.py
+++ parser/parser.py
@@ -1,2 +1,9 @@
-def parser():
+from tokenizer import tokenizer
+
+def parser(tokens):
     print("code in parser")
+    for token in tokens:
+        switch(token.type){
+         case "blabl":
+            print("parsing: blalbla");
+        }

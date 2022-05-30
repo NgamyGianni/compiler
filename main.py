bcd52b3dd8485ea1c89da4a61fe3726b5d1f1618
PHETpierre <36611009+PHETpierre@users.noreply.github.com>
2022-05-30 10:23:54 +0200

    Changes to be committed: 	modified:   main.py 	new file:   parser/expressionFactory.py 	modified:   parser/parser.py 	modified:   tokenizer/__init__.pyc 	modified:   tokenizer/tokenizer.py 	modified:   tokenizer/tokenizer.pyc

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from scoring import scoring
 file = open("test.js", "r")
 code = file.read()
 
-# consts = tokenizer.constrKeyword(s, "const")
+tokens01 = tokenizer.constrKeyword(code, "const")
 
 # print("".join([str(e.toString())+"\n" for e in consts]))
     # for const in consts:
     #     print const.toString()
 
 # tokenizer.tokenizer(f)
-parser.parser()
+tokens = [{"name" : "switch", "type" : "statementSwitch"},
+{"name":"toto", "type":"variable"}]
+print(tokens)
+parser.parser(tokens)
 scoring.scoring(code)
 
 file.close()
--- parser/parser.py
+++ parser/parser.py
@@ -1,9 +1,18 @@
 from tokenizer import tokenizer
 
+class Factory :
+    def __init__(self, type, token, start):
+        if type == "statementSwitch":
+            return self.statementSwitch(token)
+
+    def statementSwitch(token, start):
+        print('switch')
+
 def parser(tokens):
     print("code in parser")
-    for token in tokens:
-        switch(token.type){
-         case "blabl":
-            print("parsing: blalbla");
-        }
+    AST = []
+    for i in range(len(tokens)):
+        token = tokens[i]
+        if (token["type"]):
+            print("parsing: blabla");
+            Factory(token["type"], token, i)

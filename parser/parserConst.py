349cd83e4fa29008f5013cf8463704229f0f51a6
NgamyGianni <spyker97160@hotmail.fr>
2022-05-31 11:23:46 +0200

    update : parserConst

--- parser/parserConst.py
+++ parser/parserConst.py
@@ -3,3 +3,17 @@ parserConst = {
     "errorMissingOpenParenthesis" : "Error: missing opening parenthesis",
     "errorEmptyArguments" : "Error: no arguments given"
 }
+
+typeVariable= 'variable'
+typeString= 'string'
+
+expressionDeclaration= "variableDeclaration"
+expressionAffectation= "variableAffectation"
+expressionMethodCall= "objectMethodCall"
+
+declarationVariable = ["var","let", "const"]
+
+errorMissingOpenParenthesis = "Error: missing a open parenthesis"
+errorMissingCloseParenthesis= "Error: missing a close parenthesis"
+errorMissingQuotationMark= "Error: missing quotation mark"
+errorMissingWord= "Error: missing a word for valid expression"

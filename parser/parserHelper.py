3bd6fda35d9255fd1c7688ab5be5e374c6c3b536
NgamyGianni <spyker97160@hotmail.fr>
2022-05-31 11:07:44 +0200

    pending"

--- parser/parserHelper.py
+++ parser/parserHelper.py
@@ -1,4 +1,6 @@
-from parserConst import parserConst
+#from asyncio import constants
+import parserConst
+from .. import tokenizer.constants as constant
 
 def searchArgs(tokens, start):
     if tokens[start]["type"] != "openParenthese":
@@ -22,3 +24,20 @@ def searchArgs(tokens, start):
         raise NameError(parserConst["errorEmptyArguments"])
 
     return { "args" : args, "end" : end }
+
+def searchCloseCurlBrace(tokens, start):
+    countCurlBrace = 1
+    if start == len(tokens):
+        raise NameError(parserConst["errorExeceptCurlBrace"])
+    i = start
+    while i<len(tokens):
+        if tokens[i]["type"]==constant.symbolCloseCurlyBrace :#constants.symbolCloseCurlyBrace:
+            countCurlBrace-=1
+            break
+        i+=1
+    if countCurlBrace!=0:
+        raise NameError(parserConst["errorExeceptCurlBrace"])
+    end = i
+    return {"start":start, "end":end}
+
+

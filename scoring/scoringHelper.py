7295a4e81182c803af934f1456242651d9669f85
Ashanth <ashanth92@gmail.com>
2022-05-31 10:27:50 +0200

    refactor

--- scoring/scoringHelper.py
+++
@@ -1,9 +0,0 @@
-def numberLine(ast):
-    if len(ast) < 80:
-        return 5;
-    elif len(ast) < 100:
-        return 3;
-    elif len(ast) < 200:
-        return 1;
-    else:
-        return 0;

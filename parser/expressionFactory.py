1f296c103483d2980b31a5750ea5d260d645e965
NgamyGianni <spyker97160@hotmail.fr>
2022-05-31 09:16:10 +0200

    update : gianni

--- parser/expressionFactory.py
+++ parser/expressionFactory.py
@@ -1,18 +1,31 @@
+import os
+import sys
+libdir = os.path.dirname(__file__)
+sys.path.append(os.path.split(libdir)[0])
+
+from tokenizer import constants
+
 class Factory :
     def __init__(self, type, tokens, start):
         if type == "statementSwitch":
             return self.statementSwitch(tokens, start)
+        
+        if type == "variableDeclaration":
+            return self.variableDeclaration(tokens, start)
+        
+        if type == "variableAffectation":
+            return self.variableAffectation(tokens, start)
 
     def statementSwitch(self, tokens, start):
         print(tokens[start])
 
-    def variableDeclaration(tokens, start):
+    def variableDeclaration(self, tokens, start):
         if(tokens[start+1]["type"] != constants.typeWord):
             print("erreur : nom de variable non accepté")
         variableName= tokens[start+1]["value"];
         return {"type": "expressionDeclaration", "variableName": variableName};
 
-    def variableAffectation(tokens, start):
+    def variableAffectation(self, tokens, start):
         if(tokens[start-1]["type"] != constants.typeWord):
             print("erreur : nom de variable non accepté")
         variableName= tokens[start-1]["value"]
@@ -20,3 +33,4 @@ class Factory :
         return {"type": "expressionAffectation", "variableName": variableName, "variableValue": variableValue}
 
 value = [{'type': 'word', 'value': 'const'}, {'type': 'word', 'value': '_arbre1'}, {'type': 'equal', 'value': '='}, {'type': 'word', 'value': 'a'}, {'type': 'semiColon', 'value': ';'}, {'type': 'word', 'value': 'if'}, {'type': 'openParenthese', 'value': '('}, {'type': 'word', 'value': 'a'}, {'type': 'equalSame', 'value': '=='}, {'type': 'word', 'value': 'b'}, {'type': 'closeParenthese', 'value': ')'}, {'type': 'openCrochet', 'value': '{'}, {'type': 'word', 'value': 'value'}, {'type': 'equal', 'value': '='}, {'type': 'word', 'value': '2'}, {'type': 'semiColon', 'value': ';'}, {'type': 'closeCrochet', 'value': '}'}, {'type': 'word', 'value': '_arbre1'}, {'type': 'equal', 'value': '='}, {'type': 'word', 'value': 'b'}, {'type': 'semiColon', 'value': ';'}]
+

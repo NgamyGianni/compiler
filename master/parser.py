4d1e395e0ab0b99ecec842229b4813687d5615f5
NgamyGianni <spyker97160@hotmail.fr>
2022-05-31 15:23:15 +0200

    update

--- master/parser.py
+++ master/parser.py
@@ -1,6 +1,7 @@
 import expressionFactory as factory
 import tokenizer
 import parserConst
+import constants
 
 def parserFunc(AST, tokens, start, end):
 
@@ -10,17 +11,23 @@ def parserFunc(AST, tokens, start, end):
     # for i in range(len(tokens)):
         expression = None
         token = tokens[i]
+        
         if (token["type"] == "word" and token["value"] == "switch"):
             expression = factory.create(parserConst.statementSwitch, tokens, i)
         elif token["type"] == "word" and token["value"] == "case":
             expression = factory.create(parserConst.statementCase, tokens, i)
         elif token["type"] == "word" and token["value"] == parserConst.declarationFunction:
-            expression = factory.create(
-                parserConst.declarationFunction, tokens, i)
+            expression = factory.create(parserConst.declarationFunction, tokens, i)
+        elif token["type"] == "word" and token["value"] in parserConst.declarationVariable:
+            expression = factory.create(token["value"], tokens, i)
+        elif token["type"] == constants.symbolEqual:
+            expression = factory.create(token["type"], tokens, i)
+            
         if expression:
             AST.append(expression)
             i = expression["end"] 
         else:
             AST.append(tokens[i])
         i += 1
+        
     return AST
